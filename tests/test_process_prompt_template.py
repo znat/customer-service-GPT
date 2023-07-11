c6c30c87c1194313655906cf12821d61f50f4034
znat <nathan.zylbersztejn@gmail.com>
2023-07-11 16:16:14 +0200

    fix test

--- tests/test_process_prompt_template.py
+++ tests/test_process_prompt_template.py
@@ -48,11 +48,11 @@ def test_process_prompt_explain_goals_at_first_turn():
 testdata = [
     (
         [{"name": "a", "operation": "updated", "value": 10}],
-        '- User updated `a`. Aknowledge the values of `a`.',
+        '- User updated `a`. Aknowledge the values of `a`: "10".',
     ),
     (
         [{"name": "a", "operation": "added", "value": 10}],
-        '- User provided `a`. Aknowledge the values of `a`.',
+        '- User provided `a`. Aknowledge the values of `a`: "10".',
     ),
     ([], ""),
     (
@@ -60,7 +60,7 @@ testdata = [
             {"name": "a", "operation": "updated", "value": 10},
             {"name": "b", "operation": "added", "value": 20},
         ],
-        '- User provided `b` and updated `a`. Aknowledge the values of `b`, `a`.',
+        '- User provided `b` and updated `a`. Aknowledge the values of `b`: "20", `a`: "10".',
     ),
 ]
 
@@ -73,5 +73,5 @@ def test_create_output(data, expected):
         c: Optional[str] = Field(question="c")
 
     template = ProcessPromptTemplate(process=MyProcess, validate_template=False)
-    result = template.get_updates(data)
+    result = template.get_updates(data, {"a": 10, "b": 20, "c": 30})
     assert result == expected
